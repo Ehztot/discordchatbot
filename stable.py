8f5a420eff277c0ee019e24ce487622309593971
ehz <ptg0317@gmail.com>
2018-08-07 12:06:42 +0900

    add playing !help

--- stable.py
+++ stable.py
@@ -9,7 +9,7 @@ async def on_ready():
   print(client.user.name)
   print(client.user.id)
   print('------')
-
+   await client.change_presence(game=discord.Game(name=" /help "))
 @client.event
 async def on_message(message):
   if message.content.startswith('/help'):
